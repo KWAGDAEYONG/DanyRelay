cc95b9dcbaaf8063411759db0ee488b1910b1c9f
kwagdaeyong <dayong7387@hanmail.net>
2017-10-12 12:37:50 +0900

    content start

--- src/main/java/com/example/relay/controller/WorkController.java
+++ src/main/java/com/example/relay/controller/WorkController.java
@@ -1,7 +1,34 @@
 package com.example.relay.controller;
 
+import com.example.relay.model.Work;
+import com.example.relay.repository.WorkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/work")
 public class WorkController {
+
+    @Autowired
+    WorkRepository workRepository;
+
+    @GetMapping("/list")
+    public String list(Model model){
+        List<Work> workList = workRepository.findAll();
+        model.addAttribute("list",workList);
+        return "/work/list";
+    }
+
+    @GetMapping("/{id}")
+    public String getWork(@PathVariable Long id, Model model){
+        Work work = workRepository.findOne(id);
+        model.addAttribute("work",work);
+        return "/work/detail";
+    }
 }
